931ba571784530cda4ccdef6a50588caad7b2442
moise <mose.ns@hotmail.com>
2020-07-14 15:44:00 +0200

    Arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,10 @@
+//Arrays
+const friends = ['Amanda', 'Helene', 'Stewie', 'Brian', 22, undefined, null];
+console.log(friends[1]);
+friends[4] = 'Peter';
+let bestFriend = friends[2];
+console.log(bestFriend);
+console.log(friends);
+
+
+
