0773e4fa84a8680905b1234882da14f490266537
moise <mose.ns@hotmail.com>
2020-07-14 11:50:18 +0200

    variables and string

---
+++ Variables/app.js
@@ -0,0 +1,26 @@
+//Gives optiopns to store values
+
+//Undefined: no value hasn't been asigned
+//LET CONST VAR
+
+//VAR
+var value = 'something';
+console.log(value);
+
+//LET
+let firstName = 'Amanda';
+let address, zip, country;
+
+address = '555 vancouv street'
+zip = '78864';
+country = 'Canada';
+firstName = 'Amanda Tapping';
+console.log(address, zip, country);
+console.log(firstName);
+//CONST (Constant - can't be re-assign)
+const lastName = 'Magnus';
+console.log(lastName)
+
+
+
+
---
+++ string/string.js
@@ -0,0 +1,14 @@
+//'' OR ""
+const name = 'Jacouille';
+const movie = "Les visiteurs";
+const escape = 'aujourd\'hui';
+
+//String concatenation
+const leNom = 'Lafripouille';
+const lePrenom = 'Jacouille';
+let fullName = lePrenom + ' ' + leNom;
+console.log('Hello there your full name is: ' + fullName);
+
+const website = 'google';
+const url = 'https://www.' + website + '.com';
+console.log(url);
