1a349134bd96acd5ddb1f0a23ed7a902deb8418e
moise <mose.ns@hotmail.com>
2020-07-14 14:51:43 +0200

    Data type

---
+++ Datatype/datatype.js
@@ -0,0 +1,34 @@
+//DATA TYPES
+
+//PRIMITIVE: String, Number, Boolean, null, Undefined, Symbol
+//OBJECT: Arrays, Functions, Object
+
+//typeof - operator(typeof variable) (typeof value)
+
+//String
+const text = 'some text';
+console.log(typeof text);
+
+
+//Number
+const numbers = 88;
+console.log(typeof numbers);
+
+
+//Boolean
+let val1 = true;
+let val2 = false;
+console.log(typeof val1, typeof val2);
+
+
+//Null
+const results = null;
+console.log(typeof results);
+
+//Undefined
+let nom;
+console.log(typeof nom);
+
+
+//Symbol (ES6)
+
