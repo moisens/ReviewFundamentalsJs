a8d4749da6ef9324ea462f9c7095234bfaa5aeb6
moise <mose.ns@hotmail.com>
2020-07-15 14:56:14 +0200

    template literal

---
+++ TemplateLiterals/templateLit.js
@@ -0,0 +1,16 @@
+//template Literals -  ES6+
+//Backtick characters `` - above tab (left from one)
+
+const lePrenon = 'Pauline';
+const lAge = 28;
+const sentence = 'Hey it\'s ' + lePrenon + ' and she is ' + lAge + ' years old';
+
+const someVal = `Hey it's ${lePrenon} and she is ${lAge} years old.`;
+
+
+console.log(sentence);
+console.log(someVal);
+
+
+
+
