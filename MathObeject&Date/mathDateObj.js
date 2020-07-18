18e40d59613cf0787aedd9dd6683de4762b699de
moise <mose.ns@hotmail.com>
2020-07-18 09:55:13 +0200

    Math Objects

---
+++ MathObeject&Date/mathDateObj.js
@@ -0,0 +1,24 @@
+console.log('--------------Math Object-----------');
+//Math
+//Standard build-in objects - always available
+
+const leNombre = 4.56789;
+const leResult = Math.floor(leNombre);
+const leResult2 = Math.ceil(leNombre);
+const anum = 4;
+const leResult3 = Math.sqrt(anum);
+const leResult4 = Math.PI;
+const leResult5 = Math.min(1,2,3,4,5,6,7,8,9);
+const leResult6 = Math.max(1,2,3,4,5,6,7,8,9);
+
+const leResult7 = Math.floor(Math.random() * 10 + 1)
+
+
+console.log(leResult);
+console.log(leResult2);
+console.log(leResult3);
+console.log(leResult4);
+console.log(leResult5);
+console.log(leResult6);
+console.log(leResult6);
+console.log(leResult7);
