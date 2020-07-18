fa15967f7bb3fed90d3ac007fd17213f28cf283b
moise <mose.ns@hotmail.com>
2020-07-18 10:32:48 +0200

    Date

--- MathObeject&Date/mathDateObj.js
+++ MathObeject&Date/mathDateObj.js
@@ -22,3 +22,44 @@ console.log(leResult5);
 console.log(leResult6);
 console.log(leResult6);
 console.log(leResult7);
+
+console.log('-------------Date-------------');
+
+//Date
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+const date = new Date();
+const month = date.getMonth();
+console.log(months[month]);
+
+const day = date.getDay();
+console.log(days[day]);
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+const senteceDate = `${days[day]}, ${date.getDate()} ${months[month]} ${date.getFullYear()}`;
+console.log(senteceDate);
