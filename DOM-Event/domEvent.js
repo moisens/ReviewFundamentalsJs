3c27a0fce6940b9326abe9515b7cf9a0511546ee
moise <mose.ns@hotmail.com>
2020-07-19 19:08:33 +0200

    mouse event

--- DOM-Event/domEvent.js
+++ DOM-Event/domEvent.js
@@ -8,11 +8,40 @@ const heading = document.querySelector('h2')
 btn.addEventListener('click', changeColor);
 
 function changeColor(){
-    if(heading.classList.contains('red')){
-        heading.classList.remove('red');
+    if(heading.classList.contains('blue')){
+        heading.classList.remove('blue');
     }else{
-        heading.classList.add('red');
+        heading.classList.add('blue');
     }
 }
 
 
+//click - fires after ful action occurs
+//mousedown - button is pressed
+//mouseup - button is released
+//mouseenter - moved onto an element
+//mouseleave - move out of an element
+
+const headin = document.querySelector('h1');
+const btnFires = document.querySelector('.btn-fires')
+
+btnFires.addEventListener('click', function(){
+    console.log('you clicked');
+});
+
+btnFires.addEventListener('mousedown', function(){
+    console.log('down');
+});
+
+btnFires.addEventListener('mouseup', function(){
+    console.log('up');
+});
+
+
+headin.addEventListener('mouseenter', function(){
+    headin.classList.add('red')
+});
+
+headin.addEventListener('mouseleave', function(){
+    headin.classList.remove('red');
+});
